ed7730d979707ab64bd2dba07fb1e795537b6dba
Zori <zor.hay@gmail.com>
2023-10-01 00:10:51 +0400

    Added cpu usage results.

--- metrics.py
+++ metrics.py
@@ -7,10 +7,12 @@ import multiprocessing
 def measure_memory_usage(process_id, interval):
     process = psutil.Process(process_id)
     while True:
-        memory_info = process.memory_info()
-        rss_memory = memory_info.rss / (1024 * 1024)
-        with open('log.txt', 'a') as log:
-            print(f"Memory usage: {rss_memory} MB", file=log)
+        rss_memory = process.memory_info().rss / (1024 * 1024)
+        cpu_percentage = process.cpu_percent(interval)
+        with open('memory-usage.log', 'a') as memory:
+            print(f"Memory usage: {rss_memory} MB", file=memory)
+        with open('cpu-usage.log', 'a') as cpu:
+            print(f"CPU usage: {cpu_percentage} %", file=cpu)
         time.sleep(interval)
 
 
